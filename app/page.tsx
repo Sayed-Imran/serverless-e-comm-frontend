6054eb56917c955ffd23df614333ec6a3655e4cc
rahulhotta <77201683+rahulhotta@users.noreply.github.com>
2024-10-23 20:59:31 +0530

    #added httpLayer

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,25 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Loader from "./Service/Loader/Loader";
 import { useDispatch } from 'react-redux';
+import {ServiceUtils} from './Service/httpLayer'
 import { showLoader, hideLoader } from './Redux/LoaderSlice';
 export default function Home() {
   const dispatch = useDispatch();
   
+  const randomAPICall = () => {
+    try{
+      ServiceUtils.getRequest("/products/laptops",false).then((_response:any) => {
+        if (_response && _response.status === 200) {
+          console.log(_response)
+        }
+      })
+    }catch(error){
+      console.log(error)
+    }
+  }
   const startLoader = () => {
-    dispatch(showLoader());
-    setTimeout(() => {
-      dispatch(hideLoader());
-    }, 5000);
+    // dispatch(showLoader());
+    randomAPICall();
   }
   return (
     <div className={styles.page}>
