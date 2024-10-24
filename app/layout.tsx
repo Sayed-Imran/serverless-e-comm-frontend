88f2036adb210299e2ff76cd8e6c4eed3fe0b300
Surukati Asutosh Dora <asutoshdora01@gmail.com>
2024-10-24 15:04:46 +0530

    #changed folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {Providers} from "./Redux/Provider"
+import {Providers} from "./Utils/Redux/Provider"
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./Utils/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./Utils/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
-import Loader from "./Service/Loader/Loader";
+import Loader from "./Utils/Service/Loader/Loader";
 import { useDispatch } from 'react-redux';
-import {ServiceUtils} from './Service/httpLayer'
-import { showLoader, hideLoader } from './Redux/LoaderSlice';
+import {ServiceUtils} from './Utils/Service/httpLayer'
+import { showLoader, hideLoader } from './Utils/Redux/LoaderSlice';
 export default function Home() {
   const dispatch = useDispatch();
   
   const randomAPICall = () => {
     try{
-      ServiceUtils.getRequest("/products/laptops",false).then((_response:any) => {
+      ServiceUtils.getRequest("/products/laptops").then((_response:any) => {
         if (_response && _response.status === 200) {
           console.log(_response)
         }
